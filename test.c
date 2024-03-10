8045488d578c451243dbf6008f7a3ff41e6d72bb
Aashish69-96 <aashishadhikari693@gmail.com>
2024-03-10 09:36:01 +0545

    Identified different events for driver

--- test.c
+++ test.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <linux/input.h>
 
-#define MOUSEFILE "/dev/input/mice"
-
+#define MOUSEFILE "/dev/input/event11"
+//#define TOUCHPAD "/dev/input/event0"
 int main()
 {
 	int fd;
@@ -17,18 +17,9 @@ int main()
 	}
 
 	while(read(fd, &ie, sizeof(struct input_event))) {
-   // if(ie.time.tv_sec>16771976){
-    //  printf("Time for spotlight\n");
-    //}
-    if(ie.type=EV_REL && ie.code==REL_X){
-      printf("relative  moved  and wheel moved:%d\n",ie.value);
-    }
-    if(ie.type=EV_ABS){
-      printf("absolute movement\n");
-    }
-    if(ie.type=EV_KEY && ie.code==BTN_TOOL_DOUBLETAP){
-      printf("Button clicked\n");
-    }
+//    if(ie.time.tv_sec>16771976){
+//      printf("Time for spotlight");
+//    }
 		printf("time %ld.%06ld\ttype %d\tcode %d\tvalue %d\n",
 		       ie.time.tv_sec, ie.time.tv_usec, ie.type, ie.code, ie.value);
 	}
