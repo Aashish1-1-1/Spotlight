77f48d13eca73abd4f8358b7afe71606847850bd
Aashish69-96 <aashishadhikari693@gmail.com>
2024-03-11 22:07:11 +0545

    Progress somthing

--- test.c
+++ test.c
@@ -1,11 +1,21 @@
+#include <linux/input-event-codes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/input.h>
 
-#define MOUSEFILE "/dev/input/event11"
+#define MOUSEFILE "/dev/input/event9"
 //#define TOUCHPAD "/dev/input/event0"
+
+void createSpot(){
+  printf("Spotlight activated");
+}
+
+void offSpot(){
+  printf("Spotlight turned off");
+}
+
 int main()
 {
 	int fd;
@@ -17,9 +27,12 @@ int main()
 	}
 
 	while(read(fd, &ie, sizeof(struct input_event))) {
-//    if(ie.time.tv_sec>16771976){
-//      printf("Time for spotlight");
-//    }
+    if(ie.value==120){
+      createSpot(); 
+    }
+    if(ie.value==589825){
+      offSpot();
+    }
 		printf("time %ld.%06ld\ttype %d\tcode %d\tvalue %d\n",
 		       ie.time.tv_sec, ie.time.tv_usec, ie.type, ie.code, ie.value);
 	}
